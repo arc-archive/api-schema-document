db2f3afee5d68acb3f6a42db51d5e7306b3809f0
Pawel Psztyc <jarrodek@gmail.com>
2019-01-10 03:11:05 -0800

    Updating tests

--- demo/model.js
+++ demo/model.js
@@ -2,7 +2,9 @@ const generator = require('@api-components/api-model-generator');
 
 const files = new Map();
 files.set('demo-api/demo-api.raml', 'RAML 1.0');
-files.set('apic-94/apic-94.raml', 'RAML 1.0');
 
 generator(files)
-.then(() => console.log('Done.'));
+.then(() => console.log('Done.'))
+.catch((cause) => {
+  console.error(cause);
+});
